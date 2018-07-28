766da80488bbcf0e433e5830e6ec18f2b5be255a
utkarsh447 <ua.9810@gmail.com>
2018-07-28 16:54:34 +0530

    Baby Destination Task

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      host : 'ec2-54-204-46-60.compute-1.amazonaws.com',
+    	user : 'wrxudnevtlseoc',
+    	password : 'b366ac17b3b4be6d230f573106f73b9fca30bfae3345a0d426ae103ca1dcf76e',
+    	database : 'dc8c7e7vgfr235',
+    	port: 5432,
+    	ssl: true
+    }
+  }
+}
